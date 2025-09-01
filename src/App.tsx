bbb1a798c560df1724ac6d5d2bd8db0aacf3f9e8
huniversal <hunivers0523@naver.com>
2025-09-01 16:08:26 +0900

    Feat: 페이지 라우팅 임시 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BottomNav from './shared/components/common/BottomNav';
+import Home from './pages/home';
+
 function App() {
   return (
     <div className="flex justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-[480px] min-w-[320px] bg-white">
-        <h1>텅장메이트</h1>
+      <div className="w-full max-w-[480px] min-w-[320px] bg-white relative">
+        <Router>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+            <BottomNav />
+          </div>
+        </Router>
       </div>
     </div>
   )
