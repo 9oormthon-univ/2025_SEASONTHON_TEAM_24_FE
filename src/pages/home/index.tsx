f4dbf3dae7a4d272ed43895c6462d3dd500b1b0c
huniversal <hunivers0523@naver.com>
2025-09-02 23:23:52 +0900

    Feat: 홈 대시보드 전략 리스트 작업(1차) 진행

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeGoal from '../../shared/components/goal/homeGoal'
+import HomeStrategy from '../../shared/components/strategy/homeStrategy'
 
 function Home() {
   return (
     <div>
       <HomeGoal />
+      <HomeStrategy />
     </div>
   )
 }
---
+++ src/shared/components/strategy/homeStrategy.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Plus } from "lucide-react"
+
+// 전략 컴포넌트 
+function homeStrategy(){
+  function navigateToStrategyList() {
+    console.log("전략 리스트 페이지로 이동");
+    // 실제 구현시 라우터 사용 예정
+    // navigateToStrategyList('/strategies');
+  }
+  return (
+    <div className='mt-10 -mx-6'>
+      <div className='bg-primary-100 flex items-center rounded-t-12 h-[48px] px-5'>
+        <h3 className="font-semibold text-gray-900 text-20">
+          현재 진행 중인 전략
+        </h3>
+      </div>
+      <section className='px-6 py-3 bg-gray-100'>
+        <div className='h-[97px] mb-8 flex items-center bg-white rounded-12'>
+          <p className='px-5 text-left text-gray-600'>
+            진행 중인 전략이 없어요<br/>
+            이번 달도 텅장...
+          </p>
+        </div>
+        <button onClick={navigateToStrategyList} className='flex items-center justify-center w-full py-3 bg-gray-200 rounded-12'>
+          <Plus className="w-24 h-24 text-gray-700" />
+        </button>
+      </section>
+    </div>
+  )
+}
+
+export default homeStrategy
