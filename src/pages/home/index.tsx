9987e9f07d7fd14b586663199f5b3c4ab308a81e
huniversal <hunivers0523@naver.com>
2025-09-03 03:25:49 +0900

    Feat: 주간 달성 기록 1차 레이아웃 개발

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
 import HomeGoal from '../../shared/components/goal/homeGoal'
 import HomeStrategy from '../../shared/components/strategy/homeStrategy'
+import WeeklyRecord from '../../shared/components/goal/weeklyRecord'
 
 function Home() {
   return (
     <div>
       <HomeGoal />
       <HomeStrategy />
+      <WeeklyRecord />
     </div>
   )
 }
--- src/shared/components/goal/weeklyRecord.tsx
+++ src/shared/components/goal/weeklyRecord.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
 
 function weeklyRecord(){
+  const days = ['월', '화', '수', '목', '금', '토', '일'] as const;
+  const weekly:Record<string, boolean> = {
+    mon: true,   
+    tue: false, 
+    wed: true,   
+    thu: false, 
+    fri: false, 
+    sat: false, 
+    sun: false  
+  };
+  const recordKeys = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
   return (
-    <div>
-      주간 기록
+    <div className='mt-10 -mx-6'>
+      <div className='bg-primary-100 flex items-center rounded-t-12 h-[48px] px-5'>
+        <h3 className="font-semibold text-gray-900 text-20">
+          이번 주 달성 기록
+        </h3>
+      </div>
+      <section className='px-6 py-3 bg-gray-100'>
+      <div className="flex justify-between">
+        {days.map((day, index) => {
+          const isCompleted = weekly[recordKeys[index]];
+          
+          return (
+            <div key={day} className="flex flex-col items-center">
+              <span className="mb-2 text-sm text-gray-600">{day}</span>
+              {isCompleted ? 'o' : 'X'}
+            </div>
+          );
+        })}
+      </div>
+      </section>
     </div>
   )
 }
