f53f480bf042ff8481212ba74b19e4cbefe1ce2a
huniversal <hunivers0523@naver.com>
2025-09-03 21:34:00 +0900

    Style: 주간 체크 리스트 스타일 변경

--- src/shared/components/goal/weeklyRecord.tsx
+++ src/shared/components/goal/weeklyRecord.tsx
@@ -12,25 +12,28 @@ function weeklyRecord(){
   };
   const recordKeys = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
   return (
-    <div className='mt-10 -mx-6'>
-      <div className='bg-primary-100 flex items-center rounded-t-12 h-[48px] px-5'>
-        <h3 className="font-semibold text-gray-900 text-20">
-          이번 주 달성 기록
+    <div className='mt-10 -mx-[16px]'>
+      <div className='flex items-center px-5 py-8 bg-primary-100'>
+        <h3 className="font-semibold text-gray-900 text-14">
+          이번주 장독 막기
         </h3>
       </div>
-      <section className='px-6 py-3 bg-gray-100'>
-      <div className="flex justify-between">
-        {days.map((day, index) => {
-          const isCompleted = weekly[recordKeys[index]];
-          
-          return (
-            <div key={day} className="flex flex-col items-center">
-              <span className="mb-2 text-sm text-gray-600">{day}</span>
-              {isCompleted ? 'o' : 'X'}
-            </div>
-          );
-        })}
-      </div>
+      <section className='px-24 py-12 bg-white'>
+        <div className=''>
+          <p className='mb-12 text-gray-500 text-12'>이번주 막은 장독은 N개에요</p>
+        </div>
+        <div className="flex justify-between">
+          {days.map((day, index) => {
+            const isCompleted = weekly[recordKeys[index]];
+            
+            return (
+              <div key={day} className="flex flex-col items-center">
+                <span className="mb-2 text-sm text-gray-600">{day}</span>
+                {isCompleted ? 'o' : 'X'}
+              </div>
+            );
+          })}
+        </div>
       </section>
     </div>
   )
