478396e90a660594be23a817d433fbf8cab8f9a2
huniversal <hunivers0523@naver.com>
2025-09-06 19:35:01 +0900

    HotFix: login route 임시 삭제

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,21 @@
-import { Navigate } from "react-router";
-import { useAuthStore } from "../../stores/authStore";
+// import { Navigate } from "react-router";
+// import { useAuthStore } from "../../stores/authStore";
 import HomeGoal from "../../shared/components/goal/homeGoal";
 import HomeStrategy from "../../shared/components/strategy/homeStrategy";
 import WeeklyRecord from "../../shared/components/goal/weeklyRecord";
 
 function Home() {
-  const { isLoggedIn, isOnboardingCompleted } = useAuthStore()
+  // const { isLoggedIn, isOnboardingCompleted } = useAuthStore()
 
-  // 로그인하지 않은 사용자는 로그인 페이지로
-  if (!isLoggedIn) {
-    return <Navigate to="/login" replace />;
-  }
+  // // 로그인하지 않은 사용자는 로그인 페이지로
+  // if (!isLoggedIn) {
+  //   return <Navigate to="/login" replace />;
+  // }
 
-  // 로그인했지만 온보딩을 완료하지 않은 사용자는 목표 설정으로
-  if (!isOnboardingCompleted) {
-    return <Navigate to="/goal-setting" replace />;
-  }
+  // // 로그인했지만 온보딩을 완료하지 않은 사용자는 목표 설정으로
+  // if (!isOnboardingCompleted) {
+  //   return <Navigate to="/goal-setting" replace />;
+  // }
   return (
     <div>
       <HomeGoal />
